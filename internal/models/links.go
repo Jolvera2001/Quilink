d5ef9bf346ce3b94b3374da776804d7858114e37
Jolvera2001 <jolvera6@stedwards.edu>
2024-11-17 20:45:11 -0600

    more decorators

--- internal/models/links.go
+++ internal/models/links.go
@@ -4,10 +4,10 @@ import "github.com/google/uuid"
 
 type Link struct {
 	Super
-	Title  string
-	URL    string
+	Title  string `gorm:"not null"`
+	URL    string `gorm:"not null"`
 	Order  int
-	Active bool
+	Active bool `gorm:"default:true"`
 
 	// relationship
 	UserId uuid.UUID `gorm:"type:uuid;not null"`
