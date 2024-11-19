edc2924fb9d4298633443b34319628170e6706eb
Jolvera2001 <jolvera6@stedwards.edu>
2024-11-19 09:57:10 -0600

    refactored blog service and interface

--- backend/internal/components/blogComponent/blogServiceInterface.go
+++ backend/internal/components/blogComponent/blogServiceInterface.go
@@ -2,18 +2,16 @@ package blogComponent
 
 import (
 	m "quilink/internal/models"
-
-	"github.com/google/uuid"
 )
 
 type IBlogComponent interface {
-	GetBlog(id uuid.UUID) (m.Blog, error)
+	GetBlog(id m.IdRequest) (m.Blog, error)
 	GetTotalCount(profileId m.IdRequest) (int64, error)
 	GetPostBySlug(slug string) (m.Blog, error)
-	GetBlogs(profileId uuid.UUID, page, size int) ([]m.Blog, error)
-	GetPublishedBlogs(profileId uuid.UUID, page, size int) ([]m.Blog, error)
+	GetBlogs(profileId m.IdRequest, page, size int) ([]m.Blog, error)
+	GetPublishedBlogs(profileId m.IdRequest, page, size int) ([]m.Blog, error)
 	CreateBlog(dto m.BlogDto) (m.Blog, error)
-	UpdateBlog(id uuid.UUID, dto m.BlogDto) (m.Blog, error)
-	TogglePublishStatus(id uuid.UUID) error
-	DeleteBlog(id uuid.UUID) error
+	UpdateBlog(id m.IdRequest, dto m.BlogDto) (m.Blog, error)
+	TogglePublishStatus(id m.IdRequest) error
+	DeleteBlog(id m.IdRequest) error
 }
