4a41da40972792c44db1ad32e66afc64dba1904b
Jolvera2001 <jolvera6@stedwards.edu>
2024-11-20 11:45:45 -0600

    made dto and made interface

--- backend/internal/components/profileComponent/profileServiceInterface.go
+++ backend/internal/components/profileComponent/profileServiceInterface.go
@@ -1,5 +1,16 @@
 package profilecomponent
 
+import (
+	m "quilink/internal/models"
+
+	"github.com/google/uuid"
+)
+
 type IProfileService interface {
-	
+	GetProfile(id uuid.UUID) (m.Profile, error)
+	GetProfiles(UserId uuid.UUID) ([]m.Profile, error)
+	GetByDomain(customDomain string) (m.Profile, error)
+	CreateProfile(dto m.ProfileDto) (m.Profile, error)
+	UpdateProfile(id uuid.UUID, dto m.ProfileDto) (m.Profile, error) 
+	DeleteProfile(id uuid.UUID)
 }
--- backend/internal/models/profile.go
+++ backend/internal/models/profile.go
@@ -15,3 +15,12 @@ type Profile struct {
 	Blogs  []Blog    `gorm:"foreignKey:ProfileId"`
 	Link   []Link    `gorm:"foreignKey:ProfileId"`
 }
+
+type ProfileDto struct {
+	Display_name  string    `json:"display_name"`
+	Bio           string    `json:"bio"`
+	Avatar_url    string    `json:"avatar_url"`
+	Theme         string    `json:"theme"`
+	Custom_domain string    `json:"custom_domain"`
+	UserId        uuid.UUID `json:"user_id"`
+}
