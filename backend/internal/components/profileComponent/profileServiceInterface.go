e43696c2590de5f30d507ab8cad465e89ba2b12b
Jolvera2001 <jolvera6@stedwards.edu>
2024-11-21 10:25:27 -0600

    implemented profile service

--- backend/internal/components/profileComponent/profileServiceInterface.go
+++ backend/internal/components/profileComponent/profileServiceInterface.go
@@ -8,9 +8,9 @@ import (
 
 type IProfileService interface {
 	GetProfile(id uuid.UUID) (m.Profile, error)
-	GetProfiles(UserId uuid.UUID) ([]m.Profile, error)
+	GetProfiles(userId uuid.UUID) ([]m.Profile, error)
 	GetByDomain(customDomain string) (m.Profile, error)
 	CreateProfile(dto m.ProfileDto) (m.Profile, error)
 	UpdateProfile(id uuid.UUID, dto m.ProfileDto) (m.Profile, error) 
-	DeleteProfile(id uuid.UUID)
+	DeleteProfile(id uuid.UUID) error
 }
