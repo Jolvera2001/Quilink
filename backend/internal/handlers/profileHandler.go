6a44c8193562f5e2748349f22e1953627227be21
Jolvera2001 <jolvera6@stedwards.edu>
2024-11-21 13:54:38 -0600

    set up handler skeleton

--- backend/internal/handlers/profileHandler.go
+++ backend/internal/handlers/profileHandler.go
@@ -1,7 +1,58 @@
 package handlers
 
-import c "quilink/internal/components/profileComponent"
+import (
+	c "quilink/internal/components/profileComponent"
+
+	"github.com/gin-gonic/gin"
+)
 
 type ProfileHandler struct {
 	service c.IProfileService
 }
+
+func NewProfileHandler(service c.IProfileService) *ProfileHandler {
+	return &ProfileHandler{
+		service: service,
+	}
+}
+
+func GroupProfileHandlers(r *gin.Engine, h ProfileHandler) {
+	v1 := r.Group("api/v1") 
+	{
+		v1.GET("profile/:id", h.GetProfile)
+		v1.GET("profile/all/:userId", h.GetProfiles)
+		v1.GET("profile/name/:domain", h.GetByDomain)
+
+		v1.POST("profile", h.CreateProfile)
+		v1.PUT("profile", h.UpdateProfile)
+		v1.DELETE("profile/:id", h.DeleteProfile)
+	}
+}
+
+func (h *ProfileHandler) GetProfile(c *gin.Context) {
+
+}
+
+func (h *ProfileHandler) GetProfiles(c *gin.Context) {
+
+}
+
+func (h *ProfileHandler) GetByDomain(c *gin.Context) {
+
+}
+
+func (h *ProfileHandler) CreateProfile(c *gin.Context) {
+
+}
+
+func (h *ProfileHandler) UpdateProfile(c *gin.Context) {
+
+}
+
+func (h *ProfileHandler) DeleteProfile(c *gin.Context) {
+
+}
+
+
+
+
